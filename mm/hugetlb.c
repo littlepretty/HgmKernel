8250009d8c4ff94d78282949edf3eeadeb2cd3ba
James Houghton <jthoughton@google.com>
2022-03-10 18:50:13 +0000

    hugetlb: add hugetlb_pte to track HugeTLB page table entries
    After high-granularity mapping, page table entries for HugeTLB pages can
    be of any size/type. (For example, we can have a 1G page mapped with a
    mix of PMDs and PTEs.) This struct is to help keep track of a HugeTLB
    PTE after we have done a page table walk.
    
    Without this, we'd have to pass around the "size" of the PTE everywhere.
    We effectively did this before; it could be fetched from the hstate,
    which we pass around pretty much everywhere.
    
    This commit includes definitions for some basic helper functions that
    are used later. These helper functions wrap existing PTE
    inspection/modification functions, where the correct version is picked
    depending on if the HugeTLB PTE is actually "huge" or not. (Previously,
    all HugeTLB PTEs were "huge").
    
    For example, hugetlb_ptep_get wraps huge_ptep_get and ptep_get, where
    ptep_get is used when the HugeTLB PTE is PAGE_SIZE, and huge_ptep_get is
    used in all other cases.
    
    Change-Id: Ie38f41d335e220fdf4131f103932f3130d8b0408

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1118,6 +1118,34 @@ static bool vma_has_reserves(struct vm_area_struct *vma, long chg)
 	return false;
 }
 
+bool hugetlb_pte_present_leaf(const struct hugetlb_pte *hpte, pte_t pte)
+{
+	pgd_t pgd;
+	p4d_t p4d;
+	pud_t pud;
+	pmd_t pmd;
+
+	BUG_ON(!hpte->ptep);
+	switch (hugetlb_pte_level(hpte)) {
+		case HUGETLB_LEVEL_PGD:
+			pgd = __pgd(pte_val(pte));
+			return pgd_present(pgd) && pgd_leaf(pgd);
+		case HUGETLB_LEVEL_P4D:
+			p4d = __p4d(pte_val(pte));
+			return p4d_present(p4d) && p4d_leaf(p4d);
+		case HUGETLB_LEVEL_PUD:
+			pud = __pud(pte_val(pte));
+			return pud_present(pud) && pud_leaf(pud);
+		case HUGETLB_LEVEL_PMD:
+			pmd = __pmd(pte_val(pte));
+			return pmd_present(pmd) && pmd_leaf(pmd);
+		case HUGETLB_LEVEL_PTE:
+			return pte_present(pte);
+        default:
+            BUG();
+	}
+}
+
 static void enqueue_huge_page(struct hstate *h, struct page *page)
 {
 	int nid = page_to_nid(page);
