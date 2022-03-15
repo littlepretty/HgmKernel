5c25bd2cd8b1f6c1b179d2f8d65919b9465e6344
James Houghton <jthoughton@google.com>
2022-03-15 20:09:19 +0000

    hugetlb: Change huge_pte_lockptr

--- mm/page_vma_mapped.c
+++ mm/page_vma_mapped.c
@@ -174,7 +174,8 @@ bool page_vma_mapped_walk(struct page_vma_mapped_walk *pvmw)
 		if (!pvmw->pte)
 			return false;
 
-		pvmw->ptl = huge_pte_lockptr(page_hstate(page), mm, pvmw->pte);
+		pvmw->ptl = huge_pte_lockptr(huge_page_shift(page_hstate(page)),
+					     mm, pvmw->pte);
 		spin_lock(pvmw->ptl);
 		if (!check_pte(pvmw))
 			return not_found(pvmw);
