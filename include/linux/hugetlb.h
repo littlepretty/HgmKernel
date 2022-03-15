fa3a1a18bdfb63ced0a7a39820d511cd529a983a
James Houghton <jthoughton@google.com>
2022-03-15 20:09:19 +0000

    hugetlb: make huge_pte_lockptr take an explicit shift argument.
    This is needed to handle PTL locking with high-granularity mapping. We
    won't always be using the PMD-level PTL even if we're using the 2M
    hugepage hstate. It's possible that we're dealing with 4K PTEs, in which
    case, we need to lock the PTL for the 4K PTE.
    
    Change-Id: I8f72934224dbe50768d618c5cae18c065ebafb43

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -961,12 +961,11 @@ static inline gfp_t htlb_modify_alloc_mask(struct hstate *h, gfp_t gfp_mask)
 	return modified_mask;
 }
 
-static inline spinlock_t *huge_pte_lockptr(struct hstate *h,
+static inline spinlock_t *huge_pte_lockptr(unsigned int shift,
 					   struct mm_struct *mm, pte_t *pte)
 {
-	if (huge_page_size(h) == PMD_SIZE)
+	if (shift == PMD_SHIFT)
 		return pmd_lockptr(mm, (pmd_t *) pte);
-	VM_BUG_ON(huge_page_size(h) == PAGE_SIZE);
 	return &mm->page_table_lock;
 }
 
@@ -1176,7 +1175,7 @@ static inline gfp_t htlb_modify_alloc_mask(struct hstate *h, gfp_t gfp_mask)
 	return 0;
 }
 
-static inline spinlock_t *huge_pte_lockptr(struct hstate *h,
+static inline spinlock_t *huge_pte_lockptr(unsigned int shift,
 					   struct mm_struct *mm, pte_t *pte)
 {
 	return &mm->page_table_lock;
@@ -1238,7 +1237,7 @@ static inline spinlock_t *huge_pte_lock(struct hstate *h,
 {
 	spinlock_t *ptl;
 
-	ptl = huge_pte_lockptr(h, mm, pte);
+	ptl = huge_pte_lockptr(huge_page_shift(h), mm, pte);
 	spin_lock(ptl);
 	return ptl;
 }
