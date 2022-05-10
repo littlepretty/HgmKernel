6ccad3fee365724949b54661a8af4b76240f4938
James Houghton <jthoughton@google.com>
2022-05-10 20:34:26 +0000

    hugetlb: Add support for high-granularity UFFDIO_CONTINUE
    The changes here are very similar to the changes made to
    hugetlb_no_page, where we do a high-granularity page table walk and
    do accounting slightly differently because we are mapping only a piece
    of a page.
    
    Change-Id: I31d8b514bc2ce68d903ac9609326d607b0312fa7

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -218,13 +218,15 @@ unsigned long hugetlb_total_pages(void);
 vm_fault_t hugetlb_fault(struct mm_struct *mm, struct vm_area_struct *vma,
 			unsigned long address, unsigned int flags);
 #ifdef CONFIG_USERFAULTFD
-int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm, pte_t *dst_pte,
+int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm,
+				struct hugetlb_pte *dst_hpte,
 				struct vm_area_struct *dst_vma,
 				unsigned long dst_addr,
 				unsigned long src_addr,
 				enum mcopy_atomic_mode mode,
 				struct page **pagep,
-				bool wp_copy);
+				bool wp_copy,
+				bool new_mapping);
 #endif /* CONFIG_USERFAULTFD */
 bool hugetlb_reserve_pages(struct inode *inode, long from, long to,
 						struct vm_area_struct *vma,
