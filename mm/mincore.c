090c1acd5b97c8a62d4a4a4055d0dc44a7dd01a8
James Houghton <jthoughton@google.com>
2022-05-23 16:23:47 +0000

    hugetlb: use struct hugetlb_pte for walk_hugetlb_range
    Change-Id: I570bec839f2d35ac8a9fcb46795ada045453eeeb

--- mm/mincore.c
+++ mm/mincore.c
@@ -22,18 +22,19 @@
 #include <linux/uaccess.h>
 #include "swap.h"
 
-static int mincore_hugetlb(pte_t *pte, unsigned long hmask, unsigned long addr,
-			unsigned long end, struct mm_walk *walk)
+static int mincore_hugetlb(struct hugetlb_pte *hpte, unsigned long addr,
+			   struct mm_walk *walk)
 {
 #ifdef CONFIG_HUGETLB_PAGE
 	unsigned char present;
+	unsigned long end = addr + hugetlb_pte_size(hpte);
 	unsigned char *vec = walk->private;
 
 	/*
 	 * Hugepages under user process are always in RAM and never
 	 * swapped out, but theoretically it needs to be checked.
 	 */
-	present = pte && !huge_pte_none(huge_ptep_get(pte));
+	present = hpte->ptep && !huge_pte_none(huge_ptep_get(hpte->ptep));
 	for (; addr != end; vec++, addr += PAGE_SIZE)
 		*vec = present;
 	walk->private = vec;
