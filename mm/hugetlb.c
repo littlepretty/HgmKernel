43b6b48e9a0b7d382878ed7f64bc5ea06c0115de
James Houghton <jthoughton@google.com>
2022-05-24 15:04:31 +0000

    hugetlb: sort hstates in hugetlb_init_hstates
    When using HugeTLB high-granularity mapping, we need to go through the
    supported hugepage sizes in decreasing order so that we pick the largest
    size that works. Consider the case where we're faulting in a 1G hugepage
    for the first time: we want hugetlb_fault/hugetlb_no_page to map it with
    a PUD. By going through the sizes in decreasing order, we will find that
    PUD_SIZE works before finding out that PMD_SIZE or PAGE_SIZE work too.
    
    This commit also changes bootmem hugepages from storing hstate pointers
    directly to storing the hstate sizes. The hstate pointers used for
    boot-time-allocated hugepages become invalid after we sort the hstates.
    `gather_bootmem_prealloc`, called after the hstates have been sorted,
    now converts the size to the correct hstate.
    
    Change-Id: I04a96ada390cc2b0fe1221df4fe6dc5c26ea75a1

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -33,6 +33,7 @@
 #include <linux/migrate.h>
 #include <linux/nospec.h>
 #include <linux/delayacct.h>
+#include <linux/sort.h>
 
 #include <asm/page.h>
 #include <asm/pgalloc.h>
@@ -48,6 +49,10 @@
 
 int hugetlb_max_hstate __read_mostly;
 unsigned int default_hstate_idx;
+/*
+ * After hugetlb_init_hstates is called, hstates will be sorted from largest
+ * to smallest.
+ */
 struct hstate hstates[HUGE_MAX_HSTATE];
 
 #ifdef CONFIG_CMA
@@ -3017,7 +3022,7 @@ found:
 	/* Put them into a private list first because mem_map is not up yet */
 	INIT_LIST_HEAD(&m->list);
 	list_add(&m->list, &huge_boot_pages);
-	m->hstate = h;
+	m->hstate_sz = huge_page_size(h);
 	return 1;
 }
 
@@ -3031,7 +3036,7 @@ static void __init gather_bootmem_prealloc(void)
 
 	list_for_each_entry(m, &huge_boot_pages, list) {
 		struct page *page = virt_to_page(m);
-		struct hstate *h = m->hstate;
+		struct hstate *h = size_to_hstate(m->hstate_sz);
 
 		VM_BUG_ON(!hstate_is_gigantic(h));
 		WARN_ON(page_count(page) != 1);
@@ -3147,9 +3152,38 @@ static void __init hugetlb_hstate_alloc_pages(struct hstate *h)
 	kfree(node_alloc_noretry);
 }
 
+static int compare_hstates_decreasing(const void *a, const void *b)
+{
+	const int shift_a = huge_page_shift((const struct hstate *)a);
+	const int shift_b = huge_page_shift((const struct hstate *)b);
+
+	if (shift_a < shift_b)
+		return 1;
+	if (shift_a > shift_b)
+		return -1;
+	return 0;
+}
+
+static void sort_hstates(void)
+{
+	unsigned long default_hstate_sz = huge_page_size(&default_hstate);
+
+	/* Sort from largest to smallest. */
+	sort(hstates, hugetlb_max_hstate, sizeof(*hstates),
+	     compare_hstates_decreasing, NULL);
+
+	/*
+	 * We may have changed the location of the default hstate, so we need to
+	 * update it.
+	 */
+	default_hstate_idx = hstate_index(size_to_hstate(default_hstate_sz));
+}
+
 static void __init hugetlb_init_hstates(void)
 {
 	struct hstate *h, *h2;
+
+	sort_hstates();
 
 	for_each_hstate(h) {
 		/* oversize hugepages were init'ed in early boot */
