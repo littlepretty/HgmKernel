c81874afda91559076432d79de2990ff78915309
James Houghton <jthoughton@google.com>
2022-06-01 16:12:12 +0000

    hugetlb: add MADV_COLLAPSE for hugetlb
    This is a necessary extension to the UFFDIO_CONTINUE changes. When
    userspace finishes mapping an entire hugepage with UFFDIO_CONTINUE, the
    kernel has no mechanism to automatically collapse the page table to map
    the whole hugepage normally. We require userspace to inform us that they
    would like the mapping to be collapsed; they do this with MADV_COLLAPSE.
    
    If userspace has not mapped all of a hugepage with UFFDIO_CONTINUE, but
    only some, hugetlb_collapse will cause the requested range to be mapped
    as if it were UFFDIO_CONTINUE'd already. The effects of any
    UFFDIO_WRITEPROTECT calls may be undone by a call to MADV_COLLAPSE for
    intersecting address ranges.
    
    This commit is co-opting the same madvise mode that has been introduced
    to synchronously collapse THPs. The function that does THP collapsing
    has been renamed to madvise_collapse_thp.
    
    As with the rest of the high-granularity mapping support, MADV_COLLAPSE
    is only supported for shared VMAs right now.
    
    MADV_COLLAPSE for HugeTLB takes the mmap_lock for writing.
    
    It is important that we check PageHWPoison before checking
    !HPageMigratable, as PageHWPoison implies !HPageMigratable.
    !PageHWPoison && !HPageMigratable means that the page has been isolated
    for migration.
    
    Change-Id: If25dfa2dc5e0552262cf8acc15c029b61333d1a8

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -1289,6 +1289,8 @@ bool hugetlb_hgm_eligible(struct vm_area_struct *vma);
 int hugetlb_alloc_largest_pte(struct hugetlb_pte *hpte, struct mm_struct *mm,
 			      struct vm_area_struct *vma, unsigned long start,
 			      unsigned long end);
+int hugetlb_collapse(struct mm_struct *mm,
+		     unsigned long start, unsigned long end);
 #else
 static inline bool hugetlb_hgm_enabled(struct vm_area_struct *vma)
 {
@@ -1306,6 +1308,12 @@ static inline
 int hugetlb_alloc_largest_pte(struct hugetlb_pte *hpte, struct mm_struct *mm,
 			      struct vm_area_struct *vma, unsigned long start,
 			      unsigned long end)
+{
+	return -EINVAL;
+}
+static inline
+int hugetlb_collapse(struct mm_struct *mm, struct vm_area_struct *vma,
+		     unsigned long start, unsigned long end)
 {
 	return -EINVAL;
 }
