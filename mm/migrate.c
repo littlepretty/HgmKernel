74838091a4bd0bc0c70f2dd0bdeaaeca2703ef77
James Houghton <jthoughton@google.com>
2022-10-01 22:30:51 +0000

    hugetlb: remove all uses of huge_ptep_get
    Change-Id: I407869c151026db620524bd4f993eade9b3273a6

--- mm/migrate.c
+++ mm/migrate.c
@@ -328,7 +328,7 @@ void __migration_entry_wait_huge(struct hugetlb_pte *hpte, spinlock_t *ptl)
 	pte_t pte;
 
 	spin_lock(ptl);
-	pte = huge_ptep_get(hpte->ptep);
+	pte = hugetlb_pte_get(hpte);
 
 	if (unlikely(!is_hugetlb_entry_migration(pte)))
 		spin_unlock(ptl);
