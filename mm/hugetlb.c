4f802a1ee48973a2ee4c4208d2dcae735b865bd1
James Houghton <jthoughton@google.com>
2022-10-01 22:37:25 +0000

    hugetlb: remove all uses of huge_pte_clear
    Change-Id: I05ca9d52fc8315079110e3eb2235d81ffbfb99f6

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5386,8 +5386,7 @@ static void __unmap_hugepage_range(struct mmu_gather *tlb, struct vm_area_struct
 				set_huge_pte_at(mm, address, hpte.ptep,
 						make_pte_marker(PTE_MARKER_UFFD_WP));
 			else
-				huge_pte_clear(mm, address, hpte.ptep,
-						hugetlb_pte_size(&hpte));
+				hugetlb_pte_clear(mm, address, &hpte);
 			spin_unlock(ptl);
 			goto next_hpte;
 		}
@@ -6928,8 +6927,7 @@ unsigned long hugetlb_change_protection(struct vm_area_struct *vma,
 			 * no need for huge_ptep_modify_prot_start/commit().
 			 */
 			if (uffd_wp_resolve)
-				huge_pte_clear(mm, address, hpte.ptep,
-						hugetlb_pte_size(&hpte));
+				hugetlb_pte_clear(mm, address, &hpte);
 		}
 		if (!huge_pte_none(pte)) {
 			pte_t old_pte;
@@ -8017,8 +8015,7 @@ int hugetlb_collapse(struct mm_struct *mm, struct vm_area_struct *vma,
 				curr, curr + hugetlb_pte_size(&hpte),
 				curr, curr + hugetlb_pte_size(&hpte));
 		if (!hpage) {
-			huge_pte_clear(mm, curr, hpte.ptep,
-					hugetlb_pte_size(&hpte));
+			hugetlb_pte_clear(mm, curr, &hpte);
 			goto next_hpte;
 		}
 
